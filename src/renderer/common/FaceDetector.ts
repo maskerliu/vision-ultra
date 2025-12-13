5726c29a84625a5e45a983cc6b9a99ac2fc704f0
maskerliu <lynx.chrisliu@live.com>
2025-12-13 22:42:32 +0800

    修正web worker下的绘制问题

--- src/renderer/common/FaceDetector.ts
+++ src/renderer/common/FaceDetector.ts
@@ -1,7 +1,6 @@
 import { FaceLandmarker, FilesetResolver } from "@mediapipe/tasks-vision"
-import { showNotify } from "vant"
-import { baseDomain, FaceRec } from "../../common"
-import { drawTFFaceResult, FACE_DIMS, FaceResult, getFaceSlope, landmarksToFace, NUM_KEYPOINTS } from "./DrawUtils"
+import { baseDomain } from "../../common"
+import { FACE_DIMS, FaceResult, getFaceSlope, landmarksToFace, NUM_KEYPOINTS } from "./DrawUtils"
 
 
 export class FaceDetector {
@@ -13,61 +12,31 @@ export class FaceDetector {
   }
 
   private _faceRecMode: 'opencv' | 'tfjs' = 'tfjs' // opencv or tfjs
-  set faceRecMode(val: 'opencv' | 'tfjs') {
-    this._faceRecMode = val
-  }
-  private face: FaceResult = null
+  set faceRecMode(val: 'opencv' | 'tfjs') { this._faceRecMode = val }
+
+  private _face: FaceResult = null
+  get face() { return this._face }
 
   private _drawFace: boolean = true
-  set drawFace(val: boolean) {
-    this._drawFace = val
-  }
+  set drawFace(val: boolean) { this._drawFace = val }
 
   private _drawEigen: boolean = true
-  set drawEigen(val: boolean) {
-    this._drawEigen = val
-  }
-
-  private previewCtx: CanvasRenderingContext2D
-
-  private capture: HTMLCanvasElement
-  private captureCtx: CanvasRenderingContext2D
-
-  private masklayer: HTMLCanvasElement
-  private masklayerCtx: CanvasRenderingContext2D
+  set drawEigen(val: boolean) { this._drawEigen = val }
 
   private _enableFaceAngle: boolean = false
-  get enableFaceAngle() {
-    return this._enableFaceAngle
-  }
+  get enableFaceAngle() { return this._enableFaceAngle }
 
   private _faceAngle: number = 0
-  get faceAngle() {
-    return this._faceAngle
-  }
+  get faceAngle() { return this._faceAngle }
+
+  private _expire: number = 0
+  get expire(): number { return this._expire }
 
   private _isInited: boolean = false
 
-  private _expire: number = 0
-  get expire(): number {
-    return this._expire
-  }
-
-  constructor(context: CanvasRenderingContext2D, capture: HTMLCanvasElement, masklayer: HTMLCanvasElement) {
-
-    this.previewCtx = context
-    this.capture = capture
-    this.masklayer = masklayer
-
-    this.captureCtx = this.capture.getContext('2d', { willReadFrequently: true })
-    this.masklayerCtx = this.masklayer.getContext('2d', { willReadFrequently: true })
-
-  }
-
   async init() {
-    if (!this._enable) return
     this._isInited = false
-    this.face = {
+    this._face = {
       landmarks: new Float16Array(NUM_KEYPOINTS * FACE_DIMS),
       box: { xMin: 0, yMin: 0, xMax: 0, yMax: 0, width: 0, height: 0 },
       valid: false
@@ -91,8 +60,8 @@ export class FaceDetector {
     if (this.face) this.face.valid = false
     this.faceLandmarker?.close()
     this._isInited = false
-    if (this.face) this.face.landmarks = null
-    this.face = null
+    if (this._face) this._face.landmarks = null
+    this._face = null
   }
 
   set enableFaceAngle(enable: boolean) {
@@ -100,59 +69,28 @@ export class FaceDetector {
     this._faceAngle = 0
   }
 
-
-
-  updateUI() {
-    if (!this.face?.valid || !this._enable) return
-
-    drawTFFaceResult(this.previewCtx, this.face, 'none', this._drawFace, true)
-
-    if (this._drawEigen) {
-      this.captureCtx.clearRect(0, 0, this.capture.width, this.capture.height)
-      drawTFFaceResult(this.captureCtx, this.face, 'mesh', false, false, this.capture.height)
-    }
-  }
-
-
   async detect(frame: ImageData) {
-    if (!this._enable || !this._isInited) {
-      if (this.face) this.face.valid = false
-      return
-    }
+    // if (!this._enable || !this._isInited) {
+    //   if (this.face) this.face.valid = false
+    //   return
+    // }
 
     switch (this._faceRecMode) {
       case 'opencv': {
         const result = window.cvNativeApi?.faceRecognize(frame, frame.width, frame.height)
-        this.face = result?.face
+        this._face = result?.face
         // if (this.drawFace) drawCVFaceResult(this.previewCtx, this.face, this.eyes, this.landmarks)
         break
       }
       case 'tfjs': {
         let time = Date.now()
         let result = this.faceLandmarker?.detect(frame)
-        landmarksToFace(result?.faceLandmarks[0], this.face, frame.width, frame.height)
+        landmarksToFace(result?.faceLandmarks[0], this._face, frame.width, frame.height)
         this._expire = Date.now() - time
         this.calacleFaceAngle()
         break
       }
     }
-  }
-
-  async faceRec() {
-
-    // let vector = this.genFaceTensor(this.faces[0])
-    // this.capture.width = this.faces[0].box.width
-    // this.capture.height = this.faces[0].box.height
-    // await drawTFEigenFace(this.captureCtx, vector)
-    // try {
-    //   let result = await FaceRec.recognize(vector.arraySync())
-    //   return result
-    // } catch (e) {
-    //   showNotify({ type: 'warning', message: '人脸识别失败...', duration: 500 })
-    // } finally {
-    //   vector?.dispose()
-    // }
-    return null
   }
 
   private calacleFaceAngle() {
@@ -176,57 +114,67 @@ export class FaceDetector {
     // }
   }
 
+  /**
+   * 人脸采集方法
+   * @param context - CanvasRenderingContext2D对象，用于绘制图像
+   * @param name - 要注册的人脸名称
+   */
   async faceCapture(context: CanvasRenderingContext2D, name: string) {
-    if (this.face == null || this.face.landmarks.length == 0) {
-      showNotify({ type: 'warning', message: '未检测到人脸...', duration: 500 })
-      return
-    }
-
-    this.captureCtx.clearRect(0, 0, this.capture.width, this.capture.height)
-    this.masklayerCtx.clearRect(0, 0, this.masklayer.width, this.masklayer.height)
-
-    let ratio = Math.min(this.capture.width / this.face.box.width, this.capture.height / this.face.box.height)
-    this.captureCtx.drawImage(context.canvas,
-      this.face.box.xMin, this.face.box.yMin, this.face.box.width, this.face.box.height,
-      0, 0, this.face.box.width * ratio, this.face.box.height * ratio)
-    // let imageData = this.captureCtx.getImageData(0, 0, this.face.box.width * ratio, this.face.box.height * ratio)
-    // let faceOval = getFaceContour(this.face, Math.max(this.face.box.width * ratio, this.face.box.height * ratio))
-    // let region = new Path2D()
-    // region.moveTo(faceOval[0], faceOval[1])
-    // this.masklayerCtx.moveTo(faceOval[0], faceOval[1])
-    // for (let i = FACE_DIMS; i < faceOval.length; i += FACE_DIMS) {
-    //   region.lineTo(faceOval[i], faceOval[i + 1])
+    // // 检查是否检测到人脸，如果没有则提示并返回
+    // if (this.face == null || this.face.landmarks.length == 0) {
+    //   showNotify({ type: 'warning', message: '未检测到人脸...', duration: 500 })
+    //   return
     // }
-    // region.closePath()
-    // this.masklayerCtx.fillStyle = '#ff000088'
-    // this.masklayerCtx.fill(region)
-    // let maskImgData = this.masklayerCtx.getImageData(0, 0, this.masklayer.width, this.masklayer.height)
-    // for (let i = 0; i < maskImgData.data.length; i += 4) {
-    //   if (maskImgData.data[i + 3] != 136) {
-    //     maskImgData.data[i + 3] = 0
-    //   }
-    // }
-    // this.masklayerCtx.clearRect(0, 0, this.masklayer.width, this.masklayer.height)
-    // this.masklayerCtx.putImageData(maskImgData, 0, 0)
-    // this.calacleFaceAngle()
-    // this.imgProcessor.imgProcessParams['rotate'] = this._faceAngle
-    // this.imgProcessor?.process(imageData)
+
+    // // 清除画布内容
     // this.captureCtx.clearRect(0, 0, this.capture.width, this.capture.height)
-    // this.captureCtx.putImageData(imageData, 0, 0)
-    this.capture.toBlob(async (blob) => {
-      try {
-        await FaceRec.registe(name, this.face.landmarks, new File([blob], 'avatar.png', { type: 'image/png' }))
-        showNotify({ type: 'success', message: '人脸采集成功', duration: 500 })
-      } catch (err) {
-        console.error(err)
-        showNotify({ type: 'warning', message: '保存失败', duration: 500 })
-      }
+    // this.masklayerCtx.clearRect(0, 0, this.masklayer.width, this.masklayer.height)
 
-      // TODO: 保存图片 不通过请求直接native
-      // let buffer = await blob.arrayBuffer()
-      // window.mainApi?.saveFile('保存图片', `/static/face/face-${new Date().getTime()}.png`, buffer, true)
+    // // 计算缩放比例，确保人脸图像适应画布大小
+    // let ratio = Math.min(this.capture.width / this.face.box.width, this.capture.height / this.face.box.height)
+    // // 将检测到的人脸区域绘制到画布上
+    // this.captureCtx.drawImage(context.canvas,
+    //   this.face.box.xMin, this.face.box.yMin, this.face.box.width, this.face.box.height,
+    //   0, 0, this.face.box.width * ratio, this.face.box.height * ratio)
+    // // 以下代码被注释，原本用于处理人脸轮廓和蒙版效果
+    // // let imageData = this.captureCtx.getImageData(0, 0, this.face.box.width * ratio, this.face.box.height * ratio)
+    // // let faceOval = getFaceContour(this.face, Math.max(this.face.box.width * ratio, this.face.box.height * ratio))
+    // // let region = new Path2D()
+    // // region.moveTo(faceOval[0], faceOval[1])
+    // // this.masklayerCtx.moveTo(faceOval[0], faceOval[1])
+    // // for (let i = FACE_DIMS; i < faceOval.length; i += FACE_DIMS) {
+    // //   region.lineTo(faceOval[i], faceOval[i + 1])
+    // // }
+    // // region.closePath()
+    // // this.masklayerCtx.fillStyle = '#ff000088'
+    // // this.masklayerCtx.fill(region)
+    // // let maskImgData = this.masklayerCtx.getImageData(0, 0, this.masklayer.width, this.masklayer.height)
+    // // for (let i = 0; i < maskImgData.data.length; i += 4) {
+    // //   if (maskImgData.data[i + 3] != 136) {
+    // //     maskImgData.data[i + 3] = 0
+    // //   }
+    // // }
+    // // this.masklayerCtx.clearRect(0, 0, this.masklayer.width, this.masklayer.height)
+    // // this.masklayerCtx.putImageData(maskImgData, 0, 0)
+    // // this.calacleFaceAngle()
+    // // this.imgProcessor.imgProcessParams['rotate'] = this._faceAngle
+    // // this.imgProcessor?.process(imageData)
+    // // this.captureCtx.clearRect(0, 0, this.capture.width, this.capture.height)
+    // // this.captureCtx.putImageData(imageData, 0, 0)
+    // this.capture.toBlob(async (blob) => {
+    //   try {
+    //     await FaceRec.registe(name, this.face.landmarks, new File([blob], 'avatar.png', { type: 'image/png' }))
+    //     showNotify({ type: 'success', message: '人脸采集成功', duration: 500 })
+    //   } catch (err) {
+    //     console.error(err)
+    //     showNotify({ type: 'warning', message: '保存失败', duration: 500 })
+    //   }
 
-      // this.captureCtx.clearRect(0, 0, this.capture.width, this.capture.height)
-    }, 'image/png')
+    //   // TODO: 保存图片 不通过请求直接native
+    //   // let buffer = await blob.arrayBuffer()
+    //   // window.mainApi?.saveFile('保存图片', `/static/face/face-${new Date().getTime()}.png`, buffer, true)
+
+    //   // this.captureCtx.clearRect(0, 0, this.capture.width, this.capture.height)
+    // }, 'image/png')
   }
 }
--- src/renderer/tracker.worker.ts
+++ src/renderer/tracker.worker.ts
@@ -1,10 +1,10 @@
 
+import { FaceDetector } from "./common/FaceDetector"
 import { ObjectTracker } from "./common/ObjectTracker"
 
 const ctx: Worker = self as any
-
-
-let tracker: ObjectTracker = new ObjectTracker()
+let objTracker: ObjectTracker = new ObjectTracker()
+let faceDetector: FaceDetector = new FaceDetector()
 // Respond to message from parent thread
 ctx.addEventListener('message', async (event: MessageEvent<{
   type: string,
@@ -12,39 +12,59 @@ ctx.addEventListener('message', async (event: MessageEvent<{
   image?: ImageData
 }>) => {
   switch (event.data.type) {
-    case 'init':
-      await tracker.init(event.data.modelName)
-      console.log('init', tracker)
+    case 'initObjTracker':
+      await objTracker.init(event.data.modelName)
+      ctx.postMessage({ loading: true })
+      break
+    case 'initFaceDetector':
+      console.log('initFaceDetector')
+      await faceDetector.init()
+      ctx.postMessage({ loading: false })
       break
     case 'initAndDetect':
-      await tracker.init(event.data.modelName)
-      console.log('initAndDetect', tracker)
-      await tracker.detect(event.data.image)
+      await objTracker.init(event.data.modelName)
+      await objTracker.detect(event.data.image)
       ctx.postMessage({
-        boxes: tracker.boxes,
-        scores: tracker.scores,
-        classes: tracker.classes,
-        objNum: tracker.objNum,
-        scaleX: tracker.scaleX,
-        scaleY: tracker.scaleY
+        type: 'obj',
+        boxes: objTracker.boxes,
+        scores: objTracker.scores,
+        classes: objTracker.classes,
+        objNum: objTracker.objNum,
+        scaleX: objTracker.scaleX,
+        scaleY: objTracker.scaleY
       })
       break
-    case 'dispose':
-      tracker.dispose()
+    case 'objDispose':
+      objTracker.dispose()
       break
-    case 'detect':
-      console.log('detect', tracker)
-      await tracker.detect(event.data.image)
+    case 'faceDispose':
+      faceDetector.dispose()
+      break
+    case 'objDetect':
+      await objTracker.detect(event.data.image)
       ctx.postMessage({
-        boxes: tracker.boxes,
-        scores: tracker.scores,
-        classes: tracker.classes,
-        objNum: tracker.objNum,
-        scaleX: tracker.scaleX,
-        scaleY: tracker.scaleY
+        type: 'obj',
+        boxes: objTracker.boxes,
+        scores: objTracker.scores,
+        classes: objTracker.classes,
+        objNum: objTracker.objNum,
+        scaleX: objTracker.scaleX,
+        scaleY: objTracker.scaleY,
+        expire: objTracker.expire
       })
+      break
+    case 'faceDetect':
+      await faceDetector.detect(event.data.image)
+      ctx.postMessage({
+        type: 'face',
+        face: faceDetector.face,
+        expire: faceDetector.expire,
+      })
+      break
+
+    case 'faceCapture':
+      // await faceDetector.facCapture(event.data.image)
       break
   }
 
 })
-
